b9c4810cf6621291a36ecb218dd2ce768f0de792
Felipe Scalco <fscalco7@gmail.com>
2025-09-23 11:16:12 -0300

    material UI integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import { TextField } from '@mui/material'
 import './App.css'
 
 function App() {
   return (
     <main>
-      
+      <TextField id="text-input" label="Outlined" variant="outlined" />
     </main>
   )
 }
