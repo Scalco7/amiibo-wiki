2b813ffa97146b19d82346c8bd5100125e81dce3
Felipe Scalco <fscalco7@gmail.com>
2025-09-23 13:45:58 -0300

    feat: add banner / add search input

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { TextField } from '@mui/material'
+import { Button, TextField } from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search';
 import './App.css'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { AmiiboApi } from './api/amiibo-api'
 
 const amiiboApi = new AmiiboApi()
 
 function App() {
+  const [searchValue, setSearchValue] = useState('')
+  const [searcInputError, setSearchInputError] = useState(null)
+
   useEffect(() => {
     fetchAmiibos()
-    searchAmiibos()
   }, [])
 
   async function fetchAmiibos() {
@@ -19,9 +22,42 @@ function App() {
     console.log(await amiiboApi.searchAmiibos('luigi'))
   }
 
+  function onChangeTextField(event) {
+    setSearchValue(event.target.value)
+  }
+
+  function onClickSearchButton() {
+    setSearchInputError('Valor não deve ser nulo')
+    searchAmiibos(searchValue)
+  }
+
   return (
     <main>
-      <TextField id="text-input" label="Outlined" variant="outlined" />
+      <img src='amibo-banner.png' alt='banner' />
+      <div className="search-section">
+        <div className='search-input'>
+          <TextField
+            id="text-input"
+            label="Pesquisar"
+            variant="outlined"
+            value={searchValue}
+            error={searcInputError}
+            helperText={searcInputError}
+            onChange={onChangeTextField}
+            fullWidth
+          />
+        </div>
+
+        <Button
+          id="search-button"
+          variant="outlined"
+          size='large' style={{ padding: 0, height: 56, fontSize: 30 }}
+          onClick={onClickSearchButton}
+          disableElevation
+        >
+          <SearchIcon fontSize="inherit" />
+        </Button>
+      </div>
     </main>
   )
 }
