5f523cd18df2b45fcc4aad84ff8438e872fc46bb
Felipe Scalco <fscalco7@gmail.com>
2025-09-23 11:39:52 -0300

    connect with api

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import { TextField } from '@mui/material'
 import './App.css'
+import { useEffect } from 'react'
+import { AmiiboApi } from './api/amiibo-api'
+
+const amiiboApi = new AmiiboApi()
 
 function App() {
+  useEffect(() => {
+    fetchAmiibos()
+    searchAmiibos()
+  }, [])
+
+  async function fetchAmiibos() {
+    console.log(await amiiboApi.getAmiibos())
+  }
+
+  async function searchAmiibos() {
+    console.log(await amiiboApi.searchAmiibos('luigi'))
+  }
+
   return (
     <main>
       <TextField id="text-input" label="Outlined" variant="outlined" />
